a97557f53b92f050f925a5ee3caa6b0f8602b6de
Alina Vanieva <vanieva.alina@yandex.ru>
2018-03-10 03:44:14 +0300

    corrects script

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,9 @@
 (function () {
   var templates = document.querySelector("template").content;
-  var cardSmallWithImage = templates.querySelector(".card_size_s.card_content_image");
-  var cardSmallWithText = templates.querySelector(".card_size_s.card_content_text");
-  var cardMiddle = templates.querySelector(".card_size_m");
-  var cardLarge = templates.querySelector(".card_size_l");
+  var cardSmallWithImage = templates.querySelector(".small-card.small-card_content_image");
+  var cardSmallWithText = templates.querySelector(".small-card.small-card_content_text");
+  var cardMiddle = templates.querySelector(".middle-card");
+  var cardLarge = templates.querySelector(".large-card");
   var container = document.querySelector(".page__container");
 
   var documentContent = document.createDocumentFragment();
@@ -18,33 +18,45 @@
         } else if (articleData.description) {
           origin = cardSmallWithText.cloneNode(true);
         };
+        var title = origin.querySelector(".small-card__title");
+        var image = origin.querySelector(".card__picture");
+        var description = origin.querySelector(".card__description");
+        var author = origin.querySelector(".card__author");
         break
       case "m":
         origin = cardMiddle.cloneNode(true);
+        var title = origin.querySelector(".card__title");
+        var image = origin.querySelector(".card__picture");
+        var description = origin.querySelector(".card__description");
+        var author = origin.querySelector(".card__author");
         break
       case "l":
         origin = cardLarge.cloneNode(true);
+        var title = origin.querySelector(".card__title");
+        var image = origin.querySelector(".card__picture");
+        var description = origin.querySelector(".card__description");
+        var author = origin.querySelector(".card__author");
         break
       default:
         console.log("DATA ERROR: unknown size for article card");
     };
 
-    var title = origin.querySelector(".card__title");
-    var image = origin.querySelector(".card__picture");
-    var description = origin.querySelector(".card__description");
-    var author = origin.querySelector(".card__author");
+    var title = origin.querySelector("[class*=\"title\"]");
+    var image = origin.querySelector(".image");
+    var description = origin.querySelector("[class*=\"description\"]");
+    var author = origin.querySelector("[class*=\"author\"]");
 
     title.textContent = articleData.title;
     title.style.color = articleData.titleColor;
     if (image) {
       var src = articleData.image.slice(0, articleData.image.length - 4);
       var format = articleData.image.slice(articleData.image.length - 4);;
-      image.querySelector(".card__image_origin_s").setAttribute("srcset", src + format);
-      image.querySelector(".card__image_origin_m").setAttribute("srcset", src + "@2x" + format);
-      image.querySelector(".card__image_origin_l").setAttribute("srcset", src + "@3x" + format);
-      image.querySelector(".card__image_webp_s").setAttribute("srcset", src + ".webp");
-      image.querySelector(".card__image_webp_m").setAttribute("srcset", src + "@2x.webp");
-      image.querySelector(".card__image_webp_l").setAttribute("srcset", src + "@3x.webp");
+      image.querySelector(".image__origin_s").setAttribute("src", src + format);
+      image.querySelector(".image__origin_m").setAttribute("srcset", src + "@2x" + format);
+      image.querySelector(".image__origin_l").setAttribute("srcset", src + "@3x" + format);
+      image.querySelector(".image__webp_s").setAttribute("srcset", src + ".webp");
+      image.querySelector(".image__webp_m").setAttribute("srcset", src + "@2x.webp");
+      image.querySelector(".image__webp_l").setAttribute("srcset", src + "@3x.webp");
     };
     if (description) {
       description.innerHTML = articleData.description;
